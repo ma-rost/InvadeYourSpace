7c0f220508db5339af17cbfa3aff64ab3c96a833
Marissa R.S <mrowles@academic.rrc.ca>
2022-11-15 08:46:30 -0600

    Refactored DRAW_RESTRICTIONS name

--- src/globals.h
+++ src/globals.h
@@ -1,8 +1,12 @@
 ﻿#pragma once
+#include <string>
+
 #include "point.h"
 
 namespace glb
 {
-	
+	constexpr Point<int> screen_size = { 900, 900 };
+	static constexpr Point <float> draw_restrictions{ 20, screen_size.x - 30 };
+
 	const std::string title = "Invade Your Space";
 }
