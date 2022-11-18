299bfad3ba66a60ed11c47fb1c2b6ded1ce3bb54
Marissa R.S <mrowles@academic.rrc.ca>
2022-11-18 11:40:04 -0600

    New GameTemplate Class

---
+++ src/gameTemplate.cpp
@@ -0,0 +1,17 @@
+﻿#include "gameTemplate.h"
+
+void GameTemplate::update()
+{
+
+}
+
+void GameTemplate::playerDied()
+{
+	playerLivesLeft_--;
+	if (playerLivesLeft_ <= 0) isPlayerLive_ = false;
+}
+
+void GameTemplate::newGame()
+{
+
+}
---
+++ src/gameTemplate.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "enemyContainer.h"
+#include "player.h"
+
+class GameTemplate {
+	bool isPlayerLive_{ true };
+	int playerLivesLeft_{3};
+	int enemiesAlive_;
+
+	Player player{ 10, 70 };
+	EnemyContainer enemyContainer{ player };
+
+	int score_ {0};
+
+public:
+
+	void update();
+
+	void playerDied();
+	void newGame();
+	void endGame();
+};
