cf6eecb67dc8248273ca2eecb99b1151532a57ed
Marissa R.S <mrowles@academic.rrc.ca>
2022-11-21 10:28:46 -0600

    Update Sprite to be more efficient

--- src/sprite.h
+++ src/sprite.h
@@ -10,28 +10,42 @@ class Sprite {
 public:
 	ofImage drawnSprite_;
 
-	Sprite(const Point<int>& coords, const Point<int>& size);
-	Sprite(const Point<int>& coords);
-	Sprite(const int& x, const int& y);
-	Sprite();
+	Sprite(const Point <int>& coords, const Point <int>& size) :
+		spriteSize_(size) {
+		newCoords(coords);
+	}
+	Sprite(const Point <int>& coords) :
+		Sprite(coords, { 16, 8 }) {}
+	Sprite(const int& x, const int& y) :
+		Sprite({ x, y }, { 16, 8 }) {}
+	Sprite() :
+		Sprite({ 1, 49 }, { 16, 8 }) {}
 
-	static float getResize();
 	void debugSpriteVecValue() const;
+	static float getResize() { return glb::RESIZE_FACTOR; }
+	void clearCoords() { coordList_.clear(); }
+	void newCoords(const Point<int>& coords) { coordList_.emplace_back(coords); }
+	void setSize(const Point<int>& size) { spriteSize_ = size; }
 
-	void clearCoords();
-	void newCoords(const Point<int>& coords);
-	void setSize(const Point<int>& size);
-	Point<float> getSize() const;
+	Point<float> getSize() const
+	{
+		return { drawnSprite_.getWidth(), drawnSprite_.getHeight() };
+	}
 
-	// Animation
+	// Animation ------------------------------------------------------
 	void changeSprite(const int& spriteIndex, const Point<int>& coords, const Point<int>& size);
-	void changeSprite(const int& spriteIndex);
-	void changeSprite();
-	
+	void changeSprite(const int& spriteIndex)
+	{
+		changeSprite(spriteIndex, coordList_[spriteIndex], spriteSize_);
+	}
+	void changeSprite()
+	{
+		changeSprite(0, coordList_[0], spriteSize_);
+	}
+	// Draw Sprite ----------------------------------------------------
 	void drawSprite(const float& x, const float& y);
 	void drawSprite(const int& spriteIndex, const float& x, const float& y);
 
-	
-};
 
+};
 //Set new sprite. 
